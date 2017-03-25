19b0c160e61987ad967fdb3e53d94d920386ec67
roydl7 <roydl.net@gmail.com>
2017-03-25 13:43:25 +0530

    php_send_test

--- testing_send.php
+++ testing_send.php
@@ -2,8 +2,8 @@
 
 
 $servername = "10.0.4.136";
-$username = "username";
-$password = "password";
+$username = "hack";
+$password = "lol";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -14,7 +14,18 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
+$conn->select_db("trackit");
+$coords = file_get_contents("bus.pos");
+
+$result = $conn->query("SELECT * FROM bus WHERE bus_id = 1");
+if($result->num_rows > 0) {
+    $conn->query("UPDATE bus SET current_coordinates = '$coords', route_id = 1 WHERE bus_id = 1;");
+} else {
+    $conn->query("INSERT INTO bus VALUES (1,1,1,'$coords');");
+}
 
 
 
+
+printf($conn->error) ;
 ?>
