1637bbbd36f23d75bb6919b6616c4b7b4c80ed2a
roydl7 <roydl.net@gmail.com>
2017-03-25 13:22:21 +0530

    Simulator #3

---
+++ testing_send.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$servername = "10.0.4.136";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
+
+
+?>
